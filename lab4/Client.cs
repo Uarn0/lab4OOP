190ff562a4bd588932b1d24090158d315c426fb8
Uarn0 <nazarvyginny@gmail.com>
2025-05-19 20:46:57 +0300

    do class Client

---
+++ lab4/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class Client
+    {
+        public string OrganizationName { get; set; }
+        public string ResearchTopic { get; set; }
+        public int ContractValue { get; set; }
+
+        public Client() { }
+
+        public Client(string organizationName, string researchTopic, int contractValue)
+        {
+            OrganizationName = organizationName;
+            ResearchTopic = researchTopic;
+            ContractValue = contractValue;
+        }
+
+        public override string ToString()
+        {
+            return $"{OrganizationName} – {ResearchTopic} ({ContractValue} UAH)";
+        }
+    }
+}
